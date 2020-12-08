ad18b0bf7963f73abbd2130a76cffa4e27f7961d
Natalie Leung <40549274+polarcow285@users.noreply.github.com>
2020-12-07 16:31:42 -0800

    encode + decode!!

--- dartPadEncode.dart
+++ dartPadEncode.dart
@@ -1,5 +1,5 @@
 List<String> dataStringList = [];
-
+List <List> allData = [];
 class LetterSet{
   String name;
   List <String> position;
@@ -13,6 +13,9 @@ class LetterSet{
     }
     if (positionList[0] == "all"){
       position = ["beginning", "middle", "end"];
+    }
+    if (positionList[0] == "latterHalf"){
+      position = ["middle", "end"];
     }
     letters = letterList;
   }
@@ -66,13 +69,78 @@ class LetterPack{
     print (end.letters);
   }
   
+  static void encodeAll(){
+    for (int i=0; i<allPacks.length; i++){
+      List <String> temp = [];
+      allPacks[i].dataEncode(temp);
+      allData.add(temp);
+    }
+  }
+  static LetterPack decodeLetterPack(List <String> letterSetList){
+    LetterPack tempLP;
+    String letterPackName = "";
+    LetterSet tempLS1;
+    LetterSet tempLS2;
+    LetterSet tempLS3;
+    int index1;
+    int index2;
+    int index3;
+    String letterSetName1 = "";
+    String letterSetName2 = "";
+    String letterSetName3 = "";
+    List<String> position1 = [];
+    List<String> position2 = [];
+    List<String> position3 = [];
+    List<String> lettersList1 = [];
+    List<String> lettersList2 = [];
+    List<String> lettersList3 = [];
+    int count = 0;
+    
+    for(int i =0; i<letterSetList.length; i++){
+       if(letterSetList[i][0] == "#"){
+         count++;
+         if(count == 1){
+           index1 = i;
+           letterSetName1 = letterSetList[i].substring(1,letterSetList[i].length);
+           position1.add(letterSetList[i+1]);
+         }
+         if(count == 2){
+           index2 = i;
+           letterSetName2 = letterSetList[i].substring(1,letterSetList[i].length);
+           position2.add(letterSetList[i+1]);
+         }
+         if(count == 3){
+           index3 = i;
+           letterSetName3 = letterSetList[i].substring(1,letterSetList[i].length);
+           position3.add(letterSetList[i+1]);
+         }
+       }
+    }
+    letterPackName = letterSetList[0];
+    lettersList1 = letterSetList.sublist(index1+2, index2);
+    lettersList2 = letterSetList.sublist(index2+2, index3);
+    lettersList3 = letterSetList.sublist(index3+2);
+    
+    tempLS1 = new LetterSet(letterSetName1, position1, lettersList1);
+    tempLS2 = new LetterSet(letterSetName2, position2, lettersList2);
+    tempLS3 = new LetterSet(letterSetName3, position3, lettersList3);
+    tempLP = new LetterPack(letterPackName, tempLS1, tempLS2, tempLS3);
+    return tempLP;
+
+  }
+  static void decodeAll(){
+    allPacks.clear();
+    for (int i=0; i<allData.length; i++){
+      allPacks.add(decodeLetterPack(allData[i]));
+    }
+  }
   
 }
-LetterSet singleConsonantsBeginning = new LetterSet("Single Consonants", ["beginning"], ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p ", "qu", "r", "s", "t", "v", "w", "x", "y", "z"]);  
-LetterSet singleConsonantsEnding = new LetterSet("Single Consonants",["end"], ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p ", "r", "s", "t", "v", "w", "x", "y", "z"]);
+LetterSet singleConsonantsBeginning = new LetterSet("Single Consonants", ["beginning"], ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "qu", "r", "s", "t", "v", "w", "x", "y", "z"]);  
+LetterSet singleConsonantsEnding = new LetterSet("Single Consonants",["end"], ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"]);
 LetterSet hBrothers = LetterSet("H Brothers", ["sides"], ["ch", "ph", "sh", "th", "wh"]);
 LetterSet beginningBlends = LetterSet("Beginning Blends", ["beginning"], ["bl", "br", "cl", "cr", "dr", "fl", "fr", "gl", "gr", "pl", "pr", "sc", "scr", "shr", "sk", "sl", "sm", "sn", "sp", "spl", "spr", "squ", "st", "str", "sw", "thr", "tr", "tw"]);
-LetterSet shortVowelPointers = LetterSet("Short Vowel Pointers", ["middle", "end"], ["ck", "dge", "tch", "ff", "ll", "ss", "zz"]);
+LetterSet shortVowelPointers = LetterSet("Short Vowel Pointers", ["latterHalf"], ["ck", "dge", "tch", "ff", "ll", "ss", "zz"]);
 LetterSet endingBlends = LetterSet("Ending Blends", ["end"], ["sk", "sp", "st", "ct", "ft", "lk", "lt", "mp", "nch", "nd", "nt", "pt"]);
 LetterSet magicEEnding = LetterSet("Magic E", ["end"], ["be", "ce", "de", "fe", "ge", "ke", "le", "me", "ne", "pe", "se", "te"]);
 LetterSet closedSyllable = LetterSet("Closed Syllable", ["middle"], ["a", "e", "i", "o", "u"]);
@@ -104,17 +172,18 @@ LetterSet selectedEndSet;
 
 
 void main() {
-  List <List> allData = [];
-
-  for (int i=0; i<allPacks.length; i++){
-    List <String> temp = [];
-    allPacks[i].dataEncode(temp);
-    allData.add(temp);
+  for (LetterPack p in allPacks){
+    p.letterPackInfo();
   }
-  print(allData);
   
+  allPacks.add(LetterPack("askdhf", LetterSet("hi",  ["beginning"], ["hi"]), LetterSet("x", ["middle"], ["X"]), LetterSet("yz", ["beginning"], ["YZ"])));
   
+  LetterPack.encodeAll();
+  print("Encoding...");
+  LetterPack.decodeAll(); 
+  print("Decoding...");
   
- 
-  
+  for (LetterPack p in allPacks){
+    p.letterPackInfo();
+  }
 }
